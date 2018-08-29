9b4effa4806abd029c9a55651a5ac36a666e16ae
YanC Lin <yonalin1@gmail.com>
2018-08-29 19:17:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ function Scooter(year,color,model) {
   this.color = color
   this.model = model
 }
+function Driver(name,age,experience) {
+  this.name = name
+  this.age = age
+  this.experience = experience
+}
